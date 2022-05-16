808bc7bc1340267942a24a5e905c1f244d6fc5da
masud <abdullahalmasud01737906772@gmail.com>
2022-05-16 16:16:42 +0600

    another update

--- src/Pages/Dashbord/Dashbord.js
+++ src/Pages/Dashbord/Dashbord.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../Hooks/useAdmin';
 
 const Dashbord = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     return (
         <div className="drawer drawer-mobile">
             <input id="dashbord-sideber" type="checkbox" className="drawer-toggle" />
@@ -18,7 +23,7 @@ const Dashbord = () => {
                     <li><Link to='/dashbord'>My Appointment</Link> </li>
                     <li><Link to='/dashbord/review'>My Reviews</Link> </li>
                     <li><Link to='/dashbord/history'>My History</Link> </li>
-                    <li><Link to='/dashbord/allusers'>All Users</Link> </li>
+                    { admin && <li><Link to='/dashbord/allusers'>All Users</Link> </li>}
                 </ul>
 
             </div>
