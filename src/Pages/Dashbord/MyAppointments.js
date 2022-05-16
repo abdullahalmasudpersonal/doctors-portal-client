de37d92db2c2cced11f6e31ae63ace5383cab636
masud <abdullahalmasud01737906772@gmail.com>
2022-05-16 11:56:17 +0600

    another update

--- src/Pages/Dashbord/MyAppointments.js
+++ src/Pages/Dashbord/MyAppointments.js
@@ -1,22 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import {useNavigate} from 'react-router-dom';
+import {signOut} from 'firebase/auth';
+
 
 const MyAppointments = () => {
 
     const [appointments, setAppointments] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/booking?patient=${user.email}`,{
+            fetch(`http://localhost:5000/booking?patient=${user?.email}`, {
                 method: 'GET',
                 headers: {
                     'authorization': `Bearer ${localStorage.getItem('accessToken')}`
                 }
             })
-                .then(res => res.json())
-                .then(data => setAppointments(data));
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setAppointments(data);
+                });
         }
     }, [user])
 
