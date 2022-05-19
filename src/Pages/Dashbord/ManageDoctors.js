eb74c543dd67255a706aaa9655b85c425f36321d
masud <abdullahalmasud01737906772@gmail.com>
2022-05-19 22:10:28 +0600

    delete doctor

--- src/Pages/Dashbord/ManageDoctors.js
+++ src/Pages/Dashbord/ManageDoctors.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Login/Loading';
+import DeleteConfirmModal from './DeleteConfirmModal';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
-    const {data: doctors, isLoading} = useQuery('doctors', ()=> fetch('http://localhost:5000/doctor',{
+    const [deletingDoctor, setDeletingDoctor] = useState(null);
+    const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
-    }).then(res =>res.json()));
+    }).then(res => res.json()));
 
-    if(isLoading){
-        return <Loading/>
+    if (isLoading) {
+        return <Loading />
     }
 
     return (
         <div>
             <h3 className='text-2xl'>Manage Doctor: {doctors.length}</h3>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>SL</th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._id}
+                                doctor={doctor}
+                                index={index}
+                                refetch={refetch}
+                                setDeletingDoctor={setDeletingDoctor}
+                            ></DoctorRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {deletingDoctor && <DeleteConfirmModal
+                deletingDoctor={deletingDoctor}
+                setDeletingDoctor={setDeletingDoctor}
+                refetch={refetch}
+            />}
         </div>
     );
 };
